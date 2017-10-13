67ea199cdc04a48475420c9a4e8c058ed867fe4a
saoirsekearse <s.kearse2@nuigalway.ie>
2017-10-13 17:04:20 +0100

    first commit

--- src/test/java/StudentRegAssignment/StudentRegTest.java
+++ src/test/java/StudentRegAssignment/StudentRegTest.java
@@ -8,11 +8,11 @@ public class StudentRegTest extends TestCase {
 	public void testGetUserName() {
 		
 		//String StudentName = "Saoirse Kearse";
-		double StudentUsername = 114417498;
+		double StudentUsername = 14417498;
 		double expected = StudentUsername;
 		Student Student = new Student("Saoirse","30/08/1997",14417498);
-		double actual = Student.getStudentUsername();
-		assertEquals(expected,actual);
+		double result = Student.getStudentUsername();
+		assertEquals(expected, result);
     }
 	
 }
